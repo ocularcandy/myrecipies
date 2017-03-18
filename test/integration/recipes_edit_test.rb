3f634dbc1a7eda69e62398fe9bc95cede13bc08b
Amber Hayes <amber@ocularcandy.biz>
2017-03-18 08:53:23 -0500

    finish CRUD for Recipes

---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Chucky", email: "Chucky@example.com")
+    @recipe = Recipe.create(name: "Vegie Delite", description: "yummy in tummy", chef: @chef)
+  end
+
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: {recipe: {name: "", description: ""}}
+    assert_template 'recipes/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "successfully edit a recipe" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = 'Updated Recipe Name'
+    updated_description = 'updated recipe description'
+    patch recipe_path(@recipe), params: { recipe: {name: updated_name, description: updated_description}}
+    assert_redirected_to @recipe
+    #follow redirect!
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_description, @recipe.description
+  end
+  
+end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -26,6 +26,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit Recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete Recipe"
   end
 
   test "reject invalid recipe submission" do
@@ -51,4 +53,5 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match name_of_recipe.titleize, response.body
     assert_match description_of_recipe, response.body
   end
+
 end
